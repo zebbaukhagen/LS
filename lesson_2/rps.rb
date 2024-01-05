b42284820b3892f79d7e274bc89aaf7b7ea3fa4c
Zeb Baukhagen <zeb.baukhagen@gmail.com>
2024-01-04 22:36:17 -0500

    major refactoring, big fixes and improvements to game loop and string formatting

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,27 +1,41 @@
+require 'pry'
+
 VALID_CHOICES = %w((r)ock (p)aper (sc)issors (l)izard (sp)ock)
 
-WINS_AGAINST = {
-  'rock' => ['scissors', 'lizard'],
-  'paper' => ['rock', 'spock'],
-  'scissors' => ['paper', 'lizard'],
-  'lizard' => ['paper', 'spock'],
-  'spock' => ['rock', 'scissors']
+CHOICES = {
+  'rock' => { abbreviation: 'r', beats: ['scissors', 'lizard'] },
+  'paper' => { abbreviation: 'p', beats: ['rock', 'spock'] },
+  'scissors' => { abbreviation: 'sc', beats: ['paper', 'lizard'] },
+  'lizard' => { abbreviation: 'l', beats: ['paper', 'spock'] },
+  'spock' => { abbreviation: 'sp', beats: ['rock', 'scissors'] }
 }
 
-ABBREVIATIONS = {
-  'r' => 'rock',
-  'p' => 'paper',
-  'sc' => 'scissors',
-  'l' => 'lizard',
-  'sp' => 'spock'
-}
+def print_in_box(string)
+  row = (' ' * (string.size + 2)).center(string.size + 4, '|')
+  cap = ('-' * (string.size + 2)).center(string.size + 4, '+')
+
+  puts cap
+  puts row
+  puts '|' + "#{string}".center(string.size + 2) + '|'
+  puts row
+  puts cap
+end
 
 def prompt(message)
   puts "=> #{message}"
 end
 
+def clear_screen
+  system('clear')
+end
+
 def win?(player1, player2)
-  WINS_AGAINST[player1].include?(player2)
+  CHOICES[player1][:beats].include?(player2)
+end
+
+def find_key_by_abbreviation(abbreviation)
+  key = CHOICES.find { |_key, value| value[:abbreviation] == abbreviation }
+  return key.first if key
 end
 
 def display_results(winner)
@@ -52,15 +66,11 @@ def get_choice
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     user_choice = get_user_input
-    choice = ABBREVIATIONS[user_choice] || user_choice
-    return choice if valid_choice?(choice)
+    choice = find_key_by_abbreviation(user_choice)
+    choice = user_choice if choice.nil? && CHOICES.key?(user_choice)
+    return choice unless choice.nil?
     prompt("That isn't a valid choice.")
   end
-end
-
-def valid_choice?(user_choice)
-  ABBREVIATIONS.include?(user_choice) || 
-  VALID_CHOICES.any? { |choice| choice.delete('()') == user_choice }
 end
 
 def increment_score(winner, game_stats)
@@ -70,45 +80,62 @@ def increment_score(winner, game_stats)
   when 'computer'
     game_stats[:computer_score] += 1
   end
-  game_stats[:round] += 1 
+  game_stats[:round] += 1
 end
 
 def play_round(game_stats)
   choice = get_choice
-  computer_choice = VALID_CHOICES.sample.delete('()')
+  computer_choice = CHOICES.keys.sample
+  clear_screen
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
   winner = determine_results(choice, computer_choice)
   display_results(winner)
   increment_score(winner, game_stats)
-  return winner
+  winner
 end
 
 def refresh_game_prompt(game_stats)
-  game_prompt = <<MSG
-~~~~Round #{game_stats[:round]}~~~~
-Player: #{game_stats[:player_score]} 
-Computer: #{game_stats[:computer_score]}
-MSG
+  <<~MSG
+  ~~~~Round #{game_stats[:round]}~~~~
+  Player: #{game_stats[:player_score]}
+  Computer: #{game_stats[:computer_score]}
+  MSG
 end
 
-game_stats = {
-  player_score:   0,
+def display_match_end(winner, game_stats)
+  print_in_box("~~~~Match over!~~~~")
+  prompt("#{winner.capitalize} won the match!")
+  prompt("Player: #{game_stats[:player_score]} | Computer: #{game_stats[:computer_score]}")
+end
+
+def reset_game_stats
+  game_stats = {
+  player_score: 0,
   computer_score: 0,
-  round:          1
-}
+  round: 1
+  }
+end
+
+game_stats = reset_game_stats
+
+# clear screen before play starts
+clear_screen
 
 loop do # Main Loop
   until game_stats[:player_score] == 3 || game_stats[:computer_score] == 3
-    game_prompt = refresh_game_prompt(game_stats)
-    puts game_prompt
+    puts(refresh_game_prompt(game_stats))
     winner = play_round(game_stats)
   end
-  prompt("~~~~Match over!~~~~")
-  prompt("#{winner.capitalize} won the match!")
-
-  prompt "Do you want to keep playing? (y/n)"
+  display_match_end(winner, game_stats)
+  prompt "Do you want to keep playing? (y/any)"
   answer = get_user_input
-  break unless answer.start_with?('y')
+  if answer.start_with?('y')
+    game_stats = reset_game_stats
+    clear_screen
+    next
+  end
+  prompt('Thanks for playing!')
+  break
 end
