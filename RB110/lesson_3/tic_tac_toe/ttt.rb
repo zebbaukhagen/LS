72d5f1ee81a67885013fcb117ec732a25b89a434
Zeb Baukhagen <zeb.baukhagen@gmail.com>
2024-03-17 10:29:51 -0400

    finish board formatting system

--- RB110/lesson_3/tic_tac_toe/ttt.rb
+++ RB110/lesson_3/tic_tac_toe/ttt.rb
@@ -3,7 +3,7 @@ require 'yaml'
 ART = YAML.load_file('ttt_art.yml')
 MSGS = YAML.load_file('ttt_msgs.yml')
 
-TEXT_SPEED = 0.1
+TEXT_SPEED = 0.01
 
 =begin
 Tic tac toe CLI game
@@ -18,7 +18,7 @@ Features:
 6. Greeting / farewell
 7. Pause between player and computer turns to simulate computation
 8. Clear instructions screen
-9. Beautiful into/win/loss screens
+9. Beautiful intro/win/loss screens
 10. UI prompt line with info like scores/turn/etc
 11. Variable first-turn advantage (player first, computer first, computer chooses)
 12. Whoever lost last round gets to go first this round
@@ -56,17 +56,7 @@ END
 
 
 UI Design:
-
 | Round: 1 | Score: [Player: 0] [Computer: 0] | Turn: YOURS! |
-
- O | X |   
---- --- ---
-   |   |  
---- --- --- 
-    |   |  
-
-=> choose:
-
 =end
 
 def red(text)
@@ -77,19 +67,50 @@ def green(text)
   "\e[32m#{text}\e[0m"
 end
 
-board = [
-  [red('1'), red('2'), " "],
-  [" ", " ", " "],
-  [" ", " ", " "]
-]
+def set_board(size = :small)
+  case size
+    when :small
+      generate_board(3)
+    when :medium
+      generate_board(5)
+    when :large
+      generate_board(7)
+    end
+end
+
+def generate_board(rows_columns)
+  board = []
+  row = []
+  counter = 1
+  until counter > rows_columns**2
+    row << counter.to_s
+    if counter % rows_columns == 0
+      board << row
+      row = []
+    end
+    counter += 1
+  end
+  board
+end
 
 def display_board(board)
-  line = '-----------'
-  puts format(' %s | %s | %s ', board[0][0], board[0][1], board[0][2])
-  puts line
-  puts format(' %s | %s | %s ', board[1][0], board[1][1], board[1][2])
-  puts line
-  puts format(' %s | %s | %s ', board[2][0], board[2][1], board[2][2])
+  double_digit = board.last.any? { |el| el.size > 1 }
+  dash_line_size = (' ' + board.last.join(' | ') + ' ').size
+
+  board.each_with_index do |row, index|
+    puts format_row(row, double_digit)
+    if index < board.size - 1
+      puts '-' * dash_line_size
+    end
+  end
+end
+
+def format_row(row, double_digit = false)
+  formatted_row = row.map do |el|
+    colored_el = el.match?(/[1-9]/) ? red(el) : green(el)
+    double_digit && el.size < 2 ? colored_el.prepend(' ') : colored_el
+  end.join(' | ')
+  ' ' + formatted_row + ' '
 end
 
 def clear_screen
@@ -104,5 +125,27 @@ def loading_screen
   end
 end
 
-clear_screen
-loading_screen
+def make_move!(choice, marker, board)
+  board.map! do |row|
+    if row.include?(choice)
+      row.map! do |el|
+        if el == choice
+          el = marker
+        else
+          el
+        end
+      end
+    else
+      row
+    end
+  end
+end
+
+
+# row = ["8", "9", "10", "11", "12", "13", "14"]
+# puts format_row(row, true)
+
+board = set_board(:small)
+make_move!('1', 'X', board)
+make_move!('10', 'O', board)
+display_board(board)
